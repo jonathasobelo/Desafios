3197e8935d68d2f1bc4d1b561800f902e5d143d1
jonathasobelo <68344507+jonathasobelo@users.noreply.github.com>
2021-12-06 22:43:22 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,21 +1,29 @@
-function adicionar(){
+
+
+
+function adicionar() {
 
     let lista = document.getElementById("campo").value;
     let listaTarefas = document.getElementById("tarefas").innerHTML;
 
-    listaTarefas += "<li> "+lista+"</li>"
+    listaTarefas +="<input id='checar' type ='checkbox'>"+ "<button id='deletar'>Del</button>"+"<li>"+lista+ "</li>";
+
+
 
     document.getElementById("tarefas").innerHTML = listaTarefas;
 
     document.getElementById("campo").value = "";
 }
 
-document.addEventListener("keypress", function(e){
 
-    if(e.key ==="Enter"){
+
+
+document.addEventListener("keypress", function (e) {
+
+    if (e.key === "Enter") {
         const btn = document.querySelector("#btn2");
         btn.click();
     }
 
-        n      
 });
+
